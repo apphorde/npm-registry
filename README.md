4f73e6ddf6dd36340f3a9a462c81a9c3a8a69cd9
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-09-03 19:15:07 +0200

    feat: v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # npm-registry
-Minimal implementation of NPM registry format
+
+Minimal implementation of NPM registry format to use web components or ES modules via NPM
+
+## Motivation
+
+Modern applications are composed of many small components and libraries.
+It's more hassle to publish tiny packages to NPM than just maintaining them as single files.
+But as soon as you need one item in another app, a registry becomes important.
+
+This repository implements a minimal NPM-compatible, read-only, HTTP server to turn single files into NPM packages
+
+## Requirements
+
+The registry contents must follow a few rules:
+
+- every module has a scope, like `@foo`
+- every package follows semantic versioning format. Only `x.y.z` versions are allowed, for simplicity.
+- every module has a single entry file, and is an ESM module
+
+## Usage
+
+To install from the registry, NPM need a `.npmrc` configuration to tell where to search for a given scope.
+
+For example, if the registry runs at `https://npm.example.com` and we want the package `@foo/bar`, here's the `.npmrc`
+
+```
+@foo:registry=https://npm.example.com
+```
+
+With that, `npm` will look for a manifest JSON at `https://npm.example.com/@foo%2fbar`.
+The manifest will tell NPM where to download a version of `@foo/bar` from a tarball (tgz) file.
+
+## Environment variables
+
+`CACHE_PATH` defines where the tarballs will be stored. `.tgz` files will be generated automatically upon their first request.
+`DATA_PATH` must be also defined in the environment, to tell where the registry modules are stored.
+
+## Folder structure
+
+Starting from `DATA_PATH`, in this example as `/data`, here's the expected folder structure:
+
+```text
+/data
+    /@foo
+        /module-one
+            1.0.0.mjs
+            1.1.0.mjs
+
+        /module-two
+            0.1.0.mjs
+
+    /@bar
+        /bar-one
+            1.0.0.mjs
+```
---
+++ index.mjs
@@ -0,0 +1,182 @@
+import createServer from "@cloud-cli/http";
+import { pack } from "tar-stream";
+import { parse as parseJS } from "acorn";
+import { join, parse } from "node:path";
+import { createReadStream, existsSync, statSync } from "node:fs";
+import { readdir, readFile, writeFile } from "node:fs/promises";
+
+const dataDir = process.env.DATA_PATH;
+const cacheDir = process.env.CACHE_PATH;
+const validMethods = ["OPTIONS", "GET"];
+
+if (!cacheDir) {
+  throw new Error("CACHE_PATH must be defined");
+}
+
+if (!dataDir) {
+  throw new Error("DATA_PATH must be defined");
+}
+
+createServer(async function (request, response) {
+  if (validMethods.includes(request.method) === false) {
+    return notFound(response);
+  }
+
+  const host = request.headers["x-forwarded-for"];
+  const url = new URL(request.url, "http://" + host);
+
+  // NPM encodes the package name as /@foo%2fbar
+  const parts = decodeURIComponent(url.pathname).split("/");
+
+  // [@foo, bar] => manifest
+  // [@foo, bar, 0.1.0.tgz] => tarball
+  const [scope, name, requestedVersion] = parts;
+
+  if (!validateScope(scope) && validatePackageName(name)) {
+    return notFound(response);
+  }
+
+  // npm info @foo/bar
+  if (!requestedVersion) {
+    const manifest = await generateManifest(scope, name, host);
+
+    if (!manifest) {
+      return notFound(response);
+    }
+
+    response.setHeader("access-control-allow-origin", "*");
+    response.setHeader("access-control-expose-headers", "*");
+    response.setHeader("cache-control", "no-cache, no-store, max-age=0");
+    response.setHeader("content-type", "application/json");
+    response.end(JSON.stringify(manifest));
+  }
+
+  // npm install @foo/bar
+  // [workingDir]/@foo/bar/0.1.0.mjs > @foo/bar/0.1.0.tgz
+  const version = parse(requestedVersion).name;
+  if (!validateVersion(version)) {
+    return notFound(response);
+  }
+
+  const folder = join(dataDir, scope, name);
+  const file = join(folder, version + ".mjs");
+
+  if (!existsSync(file)) {
+    return notFound(response);
+  }
+
+  const content = await readFile(file, "utf-8");
+  const dependencies = await findDependencies(content);
+  const manifest = JSON.stringify({
+    name: `${scope}/${name}`,
+    version,
+    dependencies,
+    exports: "./index.mjs",
+  });
+
+  response.setHeader("content-type", "application/octet-stream");
+  response.setHeader("cache-control", "public, max-age=31536000, immutable");
+
+  const tarFile = join(cacheDir, `${scope}__${name}-${version}`);
+
+  if (!existsSync(tarFile)) {
+    const tar = pack();
+    tar.entry({ name: "package/package.json" }, manifest);
+    tar.entry({ name: "package/index.mjs" }, content);
+    tar.finalize();
+
+    const buffer = Buffer.concat(await tar.toArray());
+    await writeFile(tarfile, buffer);
+    response.end(buffer);
+    return;
+  }
+
+  createReadStream(tarFile).pipe(response);
+});
+
+async function generateManifest(scope, name, host) {
+  const folder = join(dataDir, scope, name);
+  const packageName = `${scope}/${name}`;
+  const files = (await readdir(folder, { withFileTypes: true }))
+    .filter((f) => f.isFile())
+    .map((f) => parse(f.name).name)
+    .sort();
+
+  const validVersions = files.filter((f) => f !== "latest");
+
+  if (!files.length) {
+    return null;
+  }
+
+  const versionDates = Object.fromEntries(
+    validVersions.map((v) => [
+      v,
+      new Date(statSync(join(folder, v + ".mjs")).ctimeMs).toISOString(),
+    ])
+  );
+
+  const latestVersion = validVersions[validVersions.length - 1];
+
+  return {
+    name: packageName,
+    description: "",
+    "dist-tags": {
+      latest: latestVersion,
+    },
+    versions: Object.fromEntries(
+      await Promise.all(
+        validVersions.map(async (version) => [
+          version,
+          {
+            name: packageName,
+            version,
+            description: "",
+            dist: {
+              tarball: new URL(
+                `/:npm/${scope}/${name}/${version}.tgz`,
+                "https://" + host
+              ).toString(),
+            },
+            dependencies: await findDependenciesFromFile(
+              join(folder, version + ".mjs")
+            ),
+          },
+        ])
+      )
+    ),
+    time: {
+      created: versionDates[validVersions[0]],
+      modified: versionDates[latestVersion],
+      ...versionDates,
+    },
+  };
+}
+
+async function findDependenciesFromFile(file) {
+  const content = await readFile(file, "utf8");
+  return findDependencies(content);
+}
+
+async function findDependencies(content) {
+  const ast = parseJS(content, { ecmaVersion: 2023, sourceType: "module" });
+  const imports = ast.body.filter((n) => n.type === "ImportDeclaration");
+  const names = imports.map((imp) => imp.source.value);
+
+  return Object.fromEntries(names.map((name) => [name, "latest"]));
+}
+
+function notFound(response) {
+  response.writeHead(404).end("Not found");
+}
+
+function validateScope(scope) {
+  return scope && /^@[a-z]$/.test(String(scope));
+}
+
+function validatePackageName(name) {
+  return name && /^[a-z-]$/.test(String(name));
+}
+
+function validateVersion(name) {
+  return name && /^\d+\.\d+\.\d+$/.test(String(name));
+}
